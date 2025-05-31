3814720c7fcc95d62e067679329d6d4d3d00dfd8
CJCalunia <christianjaycalunia@gmail.com>
2025-05-31 16:08:29 +0800

    Initial commit

---
+++ tests/lms_automation.spec.ts
@@ -0,0 +1,98 @@
+import {test, expect} from '@playwright/test';
+import credentials from './credentials.json';
+import { 
+    modules, 
+    submodules_requests, 
+    submodules_templates,
+    new_task_types } from './modules.js';
+
+
+
+
+test.describe('LMS Automation Tests', () => {
+    const cred = credentials[0];
+    test.beforeEach(async ({page}) => {
+        await page.goto(cred.lms_url_instructor);
+        await page.waitForSelector('text=Learning Management System - Instructor');
+        await page.waitForLoadState('load');
+        await page.getByPlaceholder('Enter Email').type(cred.lms_email_instructor, {delay: 20});
+        await page.screenshot({path: 'screenshots/login/lms_instructor_login.png'});
+        await page.getByPlaceholder('Enter Password').type(cred.default_password, {delay: 20});
+        await page.screenshot({path: 'screenshots/login/lms_instructor_password.png'});
+        await page.locator("text=Login").click();
+        await page.waitForSelector('text=Logout');
+        await page.waitForLoadState('load');
+        await page.screenshot({path: 'screenshots/login/lms_instructor_logged_in.png'});
+    });
+    test ('Checking Modules and Submodules', async ({page}) => {
+        await page.waitForLoadState('load');
+        for (const module of modules){
+           try {
+                await expect(page.getByRole('menu').getByText(module, { exact: true })).toBeVisible();
+                if (module === 'Templates') {
+                    await page.locator('text=' + module).click();    
+                    for (const submodule of submodules_templates) {
+                        try {
+                            await expect(page.getByRole('menu').getByText(submodule, { exact: true })).toBeVisible();
+                        }
+                        catch (error) {
+                            await page.screenshot({ path: `screenshots/missing_submodule/template_missing_${submodule}.png` });
+                            // Optionally, log which submodule was missing
+                            console.warn(`Template submodule missing: ${submodule}`);
+                            // Do not throw, so the test continues
+                        }
+                    }
+                }
+                if (module === 'Requests') {
+                    await page.locator('text=' + module).click();    
+                    for (const submodule of submodules_requests) {
+                        try {
+                            await expect(page.getByRole('menu').getByText(submodule, { exact: true })).toBeVisible();
+                        }
+                        catch (error) {
+                            await page.screenshot({ path: `screenshots/missing_submodule/request_missing_${submodule}.png` });
+                            // Optionally, log which submodule was missing
+                            console.warn(`Request submodule missing: ${submodule}`);
+                            // Do not throw, so the test continues
+                        }
+                    }
+                }
+            } 
+            catch (error) {
+                await page.screenshot({ path: `screenshots/missing_module/module_missing_${module}.png` });
+                // Optionally, log which module was missing
+                console.warn(`Module missing: ${module}`);
+                // Do not throw, so the test continues
+            }
+        }
+    });
+    test ('Negative Testing Task Type: New Task Type', async ({page}) => {
+        await page.waitForLoadState('load');
+        try {
+            await page.locator('text=Templates').click();
+            await page.locator('text=Task Type').click();
+            await page.locator('text=New Task Types').click();
+            await expect(page).toHaveURL("https://lms.evsu-beta.centralizedinc.com/app/faculty/templates/task-type/create");
+        }
+        catch (error) {
+            await page.screenshot({ path: 'screenshots/missing_module/template_module_missing.png' });
+            console.warn('Template module missing');
+        }
+
+ 
+        for (const taskType of new_task_types) {
+            await expect(page.locator(`//label[normalize-space()='${taskType}']`)).toBeVisible();
+   
+        }
+
+        await page.locator("//span[normalize-space()='Save']").click();
+        const requiredErrors = page.locator('.ant-form-item-explain-error', { hasText: 'required' });
+        await expect(requiredErrors).toHaveCount(5);
+        await page.waitForLoadState('load');
+        await page.screenshot({ path: 'screenshots/negative_testing/new_task_type_required.png' });
+    });
+
+
+        
+
+});
---
+++ tests/modules.js
@@ -0,0 +1,32 @@
+
+
+export const modules = [
+    "Dashboard",
+    "Classes",
+    "Student Record",
+    "Templates",
+    "Requests",
+    "Logout",
+]
+export const submodules_templates = [
+    "Task Type",
+    "Course Content",
+    "Lesson",
+    "Rubric",
+    "Questionnaire",
+    "Game",
+    "Task",
+    "Badge"
+]
+export const submodules_requests = [
+    "Excuse",
+    "Appointment"
+]
+
+export const new_task_types = [
+    "Name",
+    "Code",
+    "Tag",
+    "Answer Type",
+    "Color"
+]
